099f829b45f381851c7d8292b85b4ea5f1f33a19
varshitani29-beep <varshitani29@gmail.com>
2026-09-25 19:27:39 +0530

    Add files via upload
    Inheritance

---
+++ EXP2 C#/InheritanceProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Person
+{
+    public string Name = "Bob";
+    public int Age = 20;
+
+    public void DisplayPerson()
+    {
+        Console.WriteLine("Name : " + Name);
+        Console.WriteLine("Age : " + Age);
+    }
+}
+
+class Student : Person
+{
+    public int RollNo = 101;
+
+    public void DisplayStudent()
+    {
+        Console.WriteLine("Roll No : " + RollNo);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s = new Student();
+
+        s.DisplayPerson();
+        s.DisplayStudent();
+
+        Console.ReadLine();
+    }
+}
