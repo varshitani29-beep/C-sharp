2cf3c77802ffed60c8da7f6b51f560cefda649ef
varshitani29-beep <varshitani29@gmail.com>
2026-09-25 19:31:41 +0530

    Add files via upload
    Threading

---
+++ EXP6 C#/exp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+class ThreadDemo
+{
+ static void PrintNumbers()
+ {
+ for (int i = 1; i <= 5; i++)
+ {
+ Console.WriteLine("Child Thread: " + i);
+ Thread.Sleep(500);
+ }
+ }
+ static void Main(string[] args)
+ {
+ Thread t1 = new Thread(PrintNumbers);
+ t1.Start();
+ for (int i = 1; i <= 5; i++)
+ {
+ Console.WriteLine("Main Thread: " + i);
+ Thread.Sleep(500);
+ }
+ t1.Join();
+ Console.WriteLine("Execution Completed.");
+ }
+}
